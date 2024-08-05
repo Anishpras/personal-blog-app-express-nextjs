0373ef90394216f8c4cce2aede490262e0971191
anishpras <anishprashun118@gmail.com>
2024-08-06 02:15:15 +0530

    Alert added

--- frontend/src/app/post/[id]/page.tsx
+++ frontend/src/app/post/[id]/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 
-import { getPostById, updatePost } from "@/lib/api";
-import { notFound } from "next/navigation";
+import { getPostById, updatePost, deletePost } from "@/lib/api";
+import { notFound, useRouter } from "next/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import RichTextEditor from "@/components/RichTextEditor";
 import { Post } from "@/types";
+import { Edit, Trash2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export default function PostPage({ params }: { params: { id: string } }) {
   const [post, setPost] = useState<Post | null>(null);
@@ -15,7 +27,12 @@ export default function PostPage({ params }: { params: { id: string } }) {
   const [editedTitle, setEditedTitle] = useState("");
   const [editedContent, setEditedContent] = useState("");
   const { data: session } = useSession();
+  const [isAuthor, setIsAuthor] = useState(false);
+  const router = useRouter();
 
+  useEffect(() => {
+    session?.user?.id === post?.author?.id && setIsAuthor(true);
+  }, [post?.author?.id, session?.user?.id]);
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -52,6 +69,19 @@ export default function PostPage({ params }: { params: { id: string } }) {
     }
   };
 
+  const handleDelete = async () => {
+    try {
+      await deletePost(
+        post?.id as string,
+        //@ts-expect-error - accessToken is defined in the session object
+        session?.user?.accessToken!
+      );
+      router.push("/dashboard"); // Redirect to home page after deletion
+    } catch (error) {
+      console.error("Failed to delete post:", error);
+    }
+  };
+
   if (!post) {
     return <div>Loading...</div>;
   }
@@ -83,10 +113,44 @@ export default function PostPage({ params }: { params: { id: string } }) {
               dangerouslySetInnerHTML={{ __html: post?.content }}
             />
           )}
-          {session?.user?.id === post?.author?.id && (
-            <Button onClick={handleEdit} className="mt-4">
-              {isEditing ? "Save" : "Edit"}
-            </Button>
+          {isAuthor && (
+            <div className="flex justify-end space-x-2 mt-4">
+              <Button onClick={handleEdit} variant="outline">
+                {isEditing ? (
+                  "Save"
+                ) : (
+                  <>
+                    <Edit className="mr-2 h-4 w-4" />
+                    Edit
+                  </>
+                )}
+              </Button>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button variant="outline" className="text-red-500">
+                    <Trash2 className="mr-2 h-4 w-4" />
+                    Delete
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you absolutely sure?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete
+                      your post and remove it from our servers.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={handleDelete}>
+                      Delete
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
           )}
         </CardContent>
       </Card>
